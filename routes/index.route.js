12e2a7c25dab9f53866d2309b70b8ba792d14154
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2026-05-23 03:24:16 +0800

    feat: update welcome message to include author information

--- routes/index.route.js
+++ routes/index.route.js
@@ -13,7 +13,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   logger.debug('GET / - Welcome route')
-  res.send('Welcome to the API Library')
+  res.send('Welcome to the API Library by Adi Aryasuta (ig: @adiiaryasutaa)')
 })
 
 router.use((req, res, next) => {

b986501c6c89b0bad4b264585c6b1c034d6258d8
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2026-05-23 02:49:04 +0800

    chore: update package.json and add vercel.json for deployment
    - Added build, postinstall, and vercel-build scripts to package.json for Prisma generation.
    - Included Vercel as a dependency for deployment.
    - Created vercel.json configuration file for Vercel deployment setup.

--- index.js
+++ index.js
@@ -17,7 +17,17 @@ logger.debug('Setting up routes')
 app.use(router)
 logger.debug('Routes configured')
 
-app.listen(port, () => {
-  logger.info(`Library API is running at http://localhost:${port}`)
-  logger.info('Application started successfully')
-})
+// app.listen(port, () => {
+//   logger.info(`Library API is running at http://localhost:${port}`)
+//   logger.info('Application started successfully')
+// })
+if (process.env.ENV !== 'production') {
+  const port = process.env.PORT || 3000
+
+  app.listen(port, () => {
+    logger.info(`Library API is running at http://localhost:${port}`)
+    logger.info('Application started successfully')
+  })
+}
+
+export default app
